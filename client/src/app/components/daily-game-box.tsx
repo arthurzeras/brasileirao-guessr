74a8a7d645fa8ea856401b95d88777b04486e465
Arthur Oliveira <arthurolvmorais@gmail.com>
2023-12-29 21:22:43 -0300

    feat: add game day (number) in daily game card

--- client/src/app/components/daily-game-box.tsx
+++ client/src/app/components/daily-game-box.tsx
@@ -9,8 +9,13 @@ export default async function DailyGameBox() {
 
   return (
     <div className="bg-green-500 border border-green-600 rounded-md text-center px-4 py-6 shadow-lg text-lg">
-      Qual time ficou na {result.game.position}º posição do Brasileirão de{" "}
-      {result.game.year}?
+      <div>
+        Qual time ficou na {result.game.position}º posição do Brasileirão de{" "}
+        {result.game.year}?
+      </div>
+      <div className="text-sm italic opacity-50">
+        dia #{result.game.game_number}
+      </div>
     </div>
   );
 }
