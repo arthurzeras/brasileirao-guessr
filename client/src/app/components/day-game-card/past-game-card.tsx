aecdfa71e10dbd5ace943790b9dc23c88485d0f9
Arthur Oliveira <arthurolvmorais@gmail.com>
2024-01-02 19:22:47 -0300

    feat(client): don't use javascript action directly in JSX
    Due to the statement href="javascript:location.reload()" in past-game-card
    component, React was warning about a deprecated feature, correct
    way is to use the dangerouslySetInnerHTML prop of the JSX element.

--- client/src/app/components/day-game-card/past-game-card.tsx
+++ client/src/app/components/day-game-card/past-game-card.tsx
@@ -4,18 +4,22 @@ import { getSpecificDayGame } from "@/app/actions";
 export default async function PastGameCard({ number }: { number: string }) {
   const result = await getSpecificDayGame(number);
 
+  const reloadButton = `
+    <a href="javascript:location.reload()">
+      Recarregar
+    </a>
+  `;
+
   if (!("game" in result)) {
     return (
       <div className="flex flex-col items-center mb-8">
         <div className="text-center text-red-600 text-lg font-medium">
           Não foi possível carregar as informações do jogo.
         </div>
-        <a
-          href="javascript:location.reload()"
+        <div
           className="text-center shadow-md rounded-md py-1 mt-2 px-4 border bg-slate-300 border-slate-500"
-        >
-          Recarregar
-        </a>
+          dangerouslySetInnerHTML={{ __html: reloadButton }}
+        />
       </div>
     );
   }
